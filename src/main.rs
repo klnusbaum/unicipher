32de42586ef32deb155a7e277746880f7d5b365f
Kurtis Nusbaum <kcommiter@gmail.com>
2022-12-30 05:12:52 +0000

    stub out CLI

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,62 @@
 mod cipher;
 
 use anyhow::Result;
-use cipher::simple;
+use clap::{Args, Parser};
+use std::path::PathBuf;
+
+#[derive(clap::Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+
+    #[arg(value_enum, short, long, default_value_t = CipherType::Simple)]
+    cipher: CipherType,
+}
+
+#[derive(clap::Subcommand)]
+enum Commands {
+    Encrypt(CommandArgs),
+    Decrypt(CommandArgs),
+}
+
+#[derive(Args)]
+struct CommandArgs {
+    #[arg(short, long)]
+    input_file: Option<PathBuf>,
+
+    #[arg(short, long)]
+    output_file: Option<PathBuf>,
+
+    content: Option<String>,
+}
+
+#[derive(clap::ValueEnum, Clone)]
+enum CipherType {
+    Simple,
+    Hieroglyphs,
+}
 
 fn main() -> Result<()> {
-    let encrypted = simple::encrypt_string("adgc")?;
-    println!("Encrytped is: '{}'", encrypted);
-    let decrypted = simple::decrypt_string(&encrypted)?;
-    println!("Decrypted is: '{}'", decrypted);
+    let args = Cli::parse();
+    match args.command {
+        Commands::Encrypt(command_args) => encrypt(args.cipher, command_args),
+        Commands::Decrypt(command_args) => decrypt(args.cipher, command_args),
+    }
+    // let encrypted = simple::encrypt_string("adgc")?;
+    // println!("Encrytped is: '{}'", encrypted);
+    // let decrypted = simple::decrypt_string(&encrypted)?;
+    // println!("Decrypted is: '{}'", decrypted);
+    // Ok(())
+}
+
+fn encrypt(cipher: CipherType, args: CommandArgs) -> Result<()> {
     Ok(())
 }
 
+fn decrypt(cipher: CipherType, args: CommandArgs) -> Result<()> {
+    Ok(())
+}
 // Two ciphers.
 // One simple.
 // The other ensuring the generated unicode lands in the hieroglyphs area
