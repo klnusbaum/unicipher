4aa96b969fd42f3e139f35c37f9a7d7bf5fe1094
Kurtis Nusbaum <kcommiter@gmail.com>
2022-12-31 04:35:15 +0000

    finalize CLI

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,31 @@
 mod cipher;
 
-use anyhow::Result;
-use clap::{Args, Parser};
+use anyhow::{Error, Result};
+use cipher::{simple, Decrypt, Encrypt};
+use clap::{ArgGroup, Parser};
+use std::fs::{File, OpenOptions};
+use std::io::{stdin, stdout, BufReader, BufWriter, Cursor, Read, Write};
 use std::path::PathBuf;
 
 #[derive(clap::Parser)]
 #[command(author, version, about, long_about = None)]
+#[command(group(
+        ArgGroup::new("action")
+        .multiple(false)
+        .required(true)
+        .args(["decrypt", "encrypt"]),
+        ))]
 struct Cli {
-    #[command(subcommand)]
-    command: Commands,
+    #[arg(short, long)]
+    decrypt: bool,
+
+    #[arg(short, long)]
+    encrypt: bool,
 
     #[arg(value_enum, short, long, default_value_t = CipherType::Simple)]
     cipher: CipherType,
-}
 
-#[derive(clap::Subcommand)]
-enum Commands {
-    Encrypt(CommandArgs),
-    Decrypt(CommandArgs),
-}
-
-#[derive(Args)]
-struct CommandArgs {
-    #[arg(short, long)]
+    #[arg(short = 'f', long)]
     input_file: Option<PathBuf>,
 
     #[arg(short, long)]
@@ -39,25 +42,57 @@ enum CipherType {
 
 fn main() -> Result<()> {
     let args = Cli::parse();
-    match args.command {
-        Commands::Encrypt(command_args) => encrypt(args.cipher, command_args),
-        Commands::Decrypt(command_args) => decrypt(args.cipher, command_args),
+    args.run()
+}
+
+impl Cli {
+    fn run(&self) -> Result<()> {
+        match &self.cipher {
+            CipherType::Simple => self.with_cipher(simple::Cipher::new()),
+            CipherType::Hieroglyphs => Ok(()),
+        }
     }
-    // let encrypted = simple::encrypt_string("adgc")?;
-    // println!("Encrytped is: '{}'", encrypted);
-    // let decrypted = simple::decrypt_string(&encrypted)?;
-    // println!("Decrypted is: '{}'", decrypted);
-    // Ok(())
-}
 
-fn encrypt(cipher: CipherType, args: CommandArgs) -> Result<()> {
-    Ok(())
-}
+    fn with_cipher<C>(&self, cipher: C) -> Result<()>
+    where
+        C: Encrypt + Decrypt,
+    {
+        if let Some(content) = &self.content {
+            self.with_reader(cipher, &mut Cursor::new(content))
+        } else if let Some(file_name) = &self.input_file {
+            self.with_reader(cipher, File::open(file_name)?)
+        } else {
+            self.with_reader(cipher, stdin())
+        }
+    }
 
-fn decrypt(cipher: CipherType, args: CommandArgs) -> Result<()> {
-    Ok(())
+    fn with_reader<R, C>(&self, cipher: C, reader: R) -> Result<()>
+    where
+        C: Encrypt + Decrypt,
+        R: Read,
+    {
+        if let Some(file_name) = &self.output_file {
+            let mut file = OpenOptions::new().write(true).open(file_name)?;
+            self.with_reader_and_writer(cipher, reader, &mut file)
+        } else {
+            self.with_reader_and_writer(cipher, reader, &mut stdout())?;
+            Ok(stdout().write_all("\n".as_bytes())?)
+        }
+    }
+
+    fn with_reader_and_writer<C, R, W>(&self, cipher: C, reader: R, writer: &mut W) -> Result<()>
+    where
+        C: Encrypt + Decrypt,
+        R: Read,
+        W: Write,
+    {
+        let reader = BufReader::new(reader);
+        let mut writer = BufWriter::new(writer);
+        if self.encrypt {
+            cipher.encrypt(reader, &mut writer)
+        } else {
+            cipher.decrypt(reader, &mut writer)
+        }?;
+        Ok(writer.flush()?)
+    }
 }
-// Two ciphers.
-// One simple.
-// The other ensuring the generated unicode lands in the hieroglyphs area
-// https://en.wikipedia.org/wiki/Egyptian_Hieroglyphs_(Unicode_block)
