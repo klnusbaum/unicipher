045fc3e6786ea3b97a1824fa0247350a84e2692e
Kurtis Nusbaum <kcommiter@gmail.com>
2023-01-02 22:22:45 +0000

    add special byte pair iterator

--- src/cipher/encrypt.rs
+++ src/cipher/encrypt.rs
@@ -1,6 +1,7 @@
 use crate::cipher::Cipher;
 use anyhow::Result;
-use std::io::{Read, Write};
+use std::io::{Bytes, Read, Write};
+use std::iter::Fuse;
 
 pub struct Encrypter<W, C, const N: usize> {
     to: W,
@@ -16,14 +17,39 @@ impl<W: Write, C: Cipher<N>, const N: usize> Encrypter<W, C, N> {
     where
         R: Read,
     {
-        let mut bytes = from.bytes();
-        loop {
-            let encrypted = match (bytes.next(), bytes.next()) {
-                (Some(c0), Some(c1)) => self.cipher.encrypt_char_pair(c0?, c1?),
-                (Some(c0), None) => self.cipher.encrypt_single_char(c0?),
-                _ => return Ok(()),
+        for byte_pair in BytePairs::new(from.bytes()) {
+            let encrypted = match byte_pair {
+                (c0, Some(c1)) => self.cipher.encrypt_char_pair(c0?, c1?),
+                (c0, None) => self.cipher.encrypt_single_char(c0?),
             };
             self.to.write_all(&encrypted)?;
         }
+        Ok(())
     }
 }
+
+struct BytePairs<R> {
+    bytes: Fuse<Bytes<R>>,
+}
+
+impl<R: Read> BytePairs<R> {
+    fn new(bytes: Bytes<R>) -> Self {
+        BytePairs {
+            bytes: bytes.fuse(),
+        }
+    }
+}
+
+impl<R: Read> Iterator for BytePairs<R> {
+    type Item = BytePair;
+
+    fn next(&mut self) -> Option<BytePair> {
+        match (self.bytes.next(), self.bytes.next()) {
+            (Some(b0), Some(b1)) => Some((b0, Some(b1))),
+            (Some(b0), None) => Some((b0, None)),
+            _ => None,
+        }
+    }
+}
+
+type BytePair = (std::io::Result<u8>, Option<std::io::Result<u8>>);
