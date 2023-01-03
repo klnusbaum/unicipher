e5c294c5b55250053efef2fba7a3a2ecaa55287a
Kurtis Nusbaum <kcommiter@gmail.com>
2023-01-03 00:05:01 +0000

    clean up test helpers a bit

--- src/cipher/tests.rs
+++ src/cipher/tests.rs
@@ -7,13 +7,14 @@ where
     C: Cipher<N>,
 {
     let reader = Cursor::new(to_encrypt);
-    let buf_size = encrypt_size::<N>(to_encrypt.as_bytes().len());
+    let buf_size = encrypt_size::<N>(to_encrypt);
     let mut result = Vec::with_capacity(buf_size);
     Encrypter::new(&mut result, cipher).encrypt(reader)?;
     Ok(String::from_utf8(result)?)
 }
 
-fn encrypt_size<const N: usize>(num_bytes: usize) -> usize {
+fn encrypt_size<const N: usize>(to_encrypt: &str) -> usize {
+    let num_bytes = to_encrypt.bytes().len();
     let num_encrypted_chars_needed = if num_bytes % 2 == 0 {
         num_bytes / 2
     } else {
@@ -27,13 +28,14 @@ where
     C: Cipher<N>,
 {
     let reader = Cursor::new(to_decrypt);
-    let buf_size = decrypt_size::<N>(to_decrypt.as_bytes().len());
+    let buf_size = decrypt_size::<N>(to_decrypt);
     let mut result = Vec::with_capacity(buf_size);
     Decrypter::new(&mut result, cipher).decrypt(reader)?;
     Ok(String::from_utf8(result)?)
 }
 
-fn decrypt_size<const N: usize>(num_bytes: usize) -> usize {
+fn decrypt_size<const N: usize>(to_decrypt: &str) -> usize {
+    let num_bytes = to_decrypt.bytes().len();
     let num_encrypted_chars = num_bytes / N;
     return num_encrypted_chars * 2;
 }
