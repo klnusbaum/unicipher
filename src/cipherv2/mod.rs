84da21a102d0995543b8533dcdcb05db19e7b232
Kurtis Nusbaum <kcommiter@gmail.com>
2023-01-09 21:55:23 +0000

    add documentation around cipher v2

--- src/cipherv2/mod.rs
+++ src/cipherv2/mod.rs
@@ -13,6 +13,15 @@ pub use simple::Simple;
 
 pub type BytePair = (u8, u8);
 
+/**
+ * CipherV2 provides several major enhancements over the initial verison of Cipher:
+ *   - We don't concern ourselves with odd length strings. We just assume two chars always and
+ *   0-pad where necessary (and ignore the 0-pad where necessary).
+ *   - Encryption and Decryption targets are an actual char. This helps us ensure we're always
+ *   encrypting to a valid unicode point, as well as allowing us to write more straight forward
+ *   ciphers. We think about the code point we're encrypting/decrypting to/from rather than
+ *   thinking about the structure of utf-8 encoding.
+ */
 pub trait CipherV2 {
     fn encrypt_char_pair(&self, pair: BytePair) -> char;
     fn decrypt_char_pair(&self, encrypted: char) -> BytePair;
